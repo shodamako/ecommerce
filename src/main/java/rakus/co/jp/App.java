05b60a04d7936ff891ce8bbd071df366a44eda0d
Hiroshi Shinkai <hiroshi.shinkai@rakus.co.jp>
2015-08-25 14:28:24 +0900

    first commit

---
+++ src/main/java/rakus/co/jp/App.java
@@ -0,0 +1,13 @@
+package rakus.co.jp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
