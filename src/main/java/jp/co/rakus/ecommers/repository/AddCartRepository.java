33ba4bb5e9693b4c2d818880bd89200419d02620
Hiroshi Shinkai <hiroshi.shinkai@rakus.co.jp>
2015-08-25 16:19:35 +0900

    fix AddCartRepository

---
+++ src/main/java/jp/co/rakus/ecommers/repository/AddCartRepository.java
@@ -0,0 +1,23 @@
+package jp.co.rakus.ecommers.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AddCartRepository {
+
+//	private static final RowMapper<OrderItem> ORDER_ITEM_ROW_MAPPER = (rs , i) ->{
+//		OrderItem.set
+//		
+//		return new OrderItem();
+//	}
+//	
+//	@Autowired
+//	private NamedParameterJdbcTemplate jdbcTemplate;
+//
+//	
+//	public Item findbyId(id){
+//		
+//	}
+}
