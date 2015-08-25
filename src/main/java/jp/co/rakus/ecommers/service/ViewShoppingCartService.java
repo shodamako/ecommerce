7f2f7b147d67e6fcf14bd93bf4e0916ae798294e
Sho Kodama <sho.kodama@rakus.co.jp>
2015-08-25 18:44:47 +0900

    create cartService

---
+++ src/main/java/jp/co/rakus/ecommers/service/ViewShoppingCartService.java
@@ -0,0 +1,32 @@
+package jp.co.rakus.ecommers.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jp.co.rakus.ecommers.domain.OrderItem;
+import jp.co.rakus.ecommers.web.ViewShoppingCartPage;
+ 
+ 
+@Service
+@SessionAttributes("orderList")
+public class ViewShoppingCartService {
+	
+	
+	public ViewShoppingCartPage showCart(ArrayList<OrderItem> orderItemList){
+		return new ViewShoppingCartPage();
+	}
+	
+	
+
+
+
+
+
+
+}
+
+
+
+
