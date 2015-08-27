1292fd0a8f2c721b4d4032359763bbbe48e10b33
Sho Kodama <sho.kodama@rakus.co.jp>
2015-08-27 15:16:15 +0900

    update registerUser

--- src/main/java/jp/co/rakus/ecommers/web/RegisterUserForm.java
+++ src/main/java/jp/co/rakus/ecommers/web/RegisterUserForm.java
@@ -1,5 +1,7 @@
 package jp.co.rakus.ecommers.web;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -13,6 +15,7 @@ public class RegisterUserForm {
 	/**ユーザメールアドレス*/
 	@NotBlank(message="アドレスを入力してください")
 	@Email(message="アドレスが不正です")
+	@Size(min=8, max=16, message="パスワードは8文字以上16文字以下で登録してください")
 	private String email;
 	/**ユーザパスワード*/
 	@NotBlank(message="パスワードを入力してください")
