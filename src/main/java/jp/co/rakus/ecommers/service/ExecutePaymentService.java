63e809c5d2cd5b9d8dc1d8a64bdb6e9d6fae02ea
Masashi Ueno <masashi.ueno@rakus.co.jp>
2015-08-27 15:28:28 +0900

    注文番号を修正した

--- src/main/java/jp/co/rakus/ecommers/service/ExecutePaymentService.java
+++ src/main/java/jp/co/rakus/ecommers/service/ExecutePaymentService.java
@@ -1,7 +1,9 @@
 package jp.co.rakus.ecommers.service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,49 +23,45 @@ public class ExecutePaymentService {
 	@Autowired
 	private OrderRepository orderRepository;
 	
-	private static Integer orderNumber = 0;
 	/**
 	 * 注文情報を登録する.
 	 * @param orderItem 注文商品
 	 * @param user ユーザ情報
 	 */
 	public void insert(ArrayList<OrderItem> orderItem, User user){
-//		Calendar cal = Calendar.getInstance();
-//		Integer year = cal.get(cal.YEAR);
-//		Integer month = cal.get(cal.MONTH ) + 1;
-//		Integer day = cal.get(cal.DAY_OF_MONTH);
-//		
-//		orderNumber = 1;
-//		DecimalFormat df1 = new DecimalFormat("000000");
-//		
-//		StringBuilder sb = new StringBuilder();
-//		sb.append(year).append(month).append(day);
-//		sb.append(df1.format(orderNumber));
-//		
+		Calendar cal = Calendar.getInstance();
+		Integer year = cal.get(cal.YEAR);
+		Integer month = cal.get(cal.MONTH ) + 1;
+		Integer day = cal.get(cal.DAY_OF_MONTH);
+
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(year).append(month).append(day);
+		
 		int total = 0;
 		for(OrderItem items : orderItem){
 			total += items.getItem().getPrice() * items.getQuantity() * 1.08;
 		}
 		Order order = new Order();
-//		order.setUserId(user.getId());
+		order.setUserId((long)2);
 		order.setUserId((long)2);
 		order.setStatus(1);
-		order.setOrderNumber("1");
+		order.setOrderNumber(sb.toString());
 		order.setTotalPrice(total);
 		order.setOrderItemList(orderItem);
 		
 		orderRepository.insertOrder(order);
 		
 		
-		for(OrderItem item : orderItem){
-			orderRepository.insertOrderItem(item);
-		}
+//		for(OrderItem item : orderItem){
+//			orderRepository.insertOrderItem(item);
+//		}
 		
-//		
-//		OrderItem item = new OrderItem();
-//		item.setItemId((long)1);
-//		item.setOrderId((long)1);
-//		item.setQuantity(3);
-//		orderRepository.insertOrderItem(item);
+		
+		OrderItem item = new OrderItem();
+		item.setItemId((long)1);
+		item.setOrderId((long)1);
+		item.setQuantity(3);
+		orderRepository.insertOrderItem(item);
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/tesut.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/executePayment/insert" method="post">
+		<br> <input type="submit" value="確定">
+	</form>
+</body>
+</html>
