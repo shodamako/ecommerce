d1c7f35f3f26a6cbc466eae1c2f84dbaa9062561
Takayuki Honma <takayuki.honma@rakus.co.jp>
2015-08-28 10:30:47 +0900

    Ordersテーブル書きこみ処理簡略化

--- src/main/java/jp/co/rakus/ecommers/repository/OrderRepository.java
+++ src/main/java/jp/co/rakus/ecommers/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jp.co.rakus.ecommers.repository;
 
 //<<<<<<< HEAD
 import java.sql.Date;
+import java.text.DecimalFormat;
 import java.util.List;
 //=======
 //import java.util.Date;
@@ -34,15 +35,6 @@ public class OrderRepository {
 	private SimpleJdbcInsert insert;
 	private static Long orderId;
 	
-//	private static final RowMapper<Order> ORDER_ROW_MAPPER = (rs, i) -> {
-//		Long id = rs.getLong("id");
-//		String orderNumber = rs.getString("order_number");
-//		Long userId = rs.getLong("user_id");
-//		Integer status = rs.getInt("status");
-//		Integer totalPrice = rs.getInt("total_price");
-//		return new Order(id, orderNumber, userId, status, null, totalPrice);
-//	};
-	
 	@PostConstruct
 	public void init(){
 		insert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations()).withTableName("orders").usingGeneratedKeyColumns("id");
@@ -53,22 +45,22 @@ public class OrderRepository {
 	 * @param order 注文情報
 	 */
 	public void insertOrder(Order order){
+		StringBuilder sb = new StringBuilder();
+		String orderNum = order.getOrderNumber();
+		DecimalFormat dformat = new DecimalFormat("000000");
+		sb.append(orderNum).append(dformat.format(selectNexstSeq()));
+		order.setOrderNumber(sb.toString());
+		
 		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
 		Number key = insert.executeAndReturnKey(param);
 		orderId = key.longValue();
-		
-		order.setId(orderId);
-		param = new BeanPropertySqlParameterSource(order);
-		jdbcTemplate.update("DELETE FROM orders WHERE id=:id", param);
-		jdbcTemplate.update("INSERT INTO orders(order_number, user_id, status, total_price, date) values(:orderNumber || to_char(nextval('order_number_count'),'000000'), :userId, :status, :totalPrice,:date)", param);
-		++orderId;
 	}
 	
-	
-	public Order getSequence(Order order){
-		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
-		Order item = jdbcTemplate.queryForObject("SELECT id, order_number, user_id, status, total_price FROM orders;", param, ORDER_ROW_MAPPER);
-		return item;
+	public Integer selectNexstSeq() {
+		String sql = "SELECT nextval('order_number_count')";
+		SqlParameterSource param = new MapSqlParameterSource();
+		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+		return result;
 	}
 	
 	/**
